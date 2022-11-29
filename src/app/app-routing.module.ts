953a2778a96547a9a803a9f8088ae32aeb9d3cfb
Plamen Simeonov <p.simeonov@Micromet.local>
2022-11-29 16:18:10 +0200

    added routing and company and 404 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./_components/home/home.component";
+import {PageNotFoundComponent} from "./_shared/page-not-found/page-not-found.component";
+import {CompanyComponent} from "./_components/company/company.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'company', component: CompanyComponent},
+  {path: '', redirectTo: '/home', pathMatch: "full"},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./_shared/header/header.component";
 import {FooterComponent} from "./_shared/footer/footer.component";
-import {HomeComponent} from "./_views/home/home.component";
+import {HomeComponent} from "./_components/home/home.component";
+import { BackgroundComponent } from './_shared/background/background.component';
+import { ProductCardComponent } from './_components/product-card/product-card.component';
+import { ProductsComponent } from './_components/products/products.component';
+import { PageNotFoundComponent } from './_shared/page-not-found/page-not-found.component';
+import { CompanyComponent } from './_components/company/company.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    BackgroundComponent,
+    ProductCardComponent,
+    ProductsComponent,
+    PageNotFoundComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
